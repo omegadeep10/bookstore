81fe8ceba20c022446a47a7927570c3e750cfcce
Deep Patel <dpatel@stins.com>
2016-07-20 15:49:52 -0400

    Init heroku commit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
-var port = 3000;
+var port = process.env.PORT || 5000;
 
-app.use(express.static('public'));
-app.use(express.static('src/views'));
 
-app.listen(300, function(err) {
+app.listen(port, function(err) {
     console.log('Running server on port: ' + port);
+});
+
+app.get('/', function(req, res) {
+	res.send('Hello World!');
 });
