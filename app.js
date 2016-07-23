2184867e4eca7128872ace6f35a383038ab4e1d8
omegadeep10 <omegadeep10@outlook.com>
2016-07-23 16:28:11 -0400

    Working mysql connection

--- app.js
+++ app.js
@@ -2,51 +2,28 @@ var express = require('express');
 var mysql = require('mysql');
 
 var app = express();
+var port = process.env.PORT || 5000;
 
-var db_config = {
+app.use(express.static('public'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+var pool = mysql.createPool({
+    connectionLimit: 5,
     host: 'us-cdbr-iron-east-04.cleardb.net',
     user: 'b13e6ef2d95564',
     password: 'b3f85481',
     database: 'heroku_fb65db9133555c5'
-};
-
-var connection;
-
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config);
-
-  connection.connect(function(err) {          
-    if(err) {             
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); 
-    }                                     
-  });                                     
-                    
-  connection.on('error', function(err) {
-    console.log('DB ERROR: ', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleDisconnect();                         
-    } 
-    else {                                 
-      throw err;                                 
-    }
-  });
-}
-
-handleDisconnect();
+});
 
 app.get('/', function(req, res) {
-    connection.query('SELECT * FROM books', function(err, rows, fields){
+    pool.query('SELECT * FROM books', function(err, rows, fields){
         if (err) {
             console.log("Connection error: ", err);
         }
-        res.send(['Hello World!', rows]);
+        res.render('index', {data: rows});
     });
 });
-
-
-
-var port = process.env.PORT || 5000;
 
 app.listen(port, function(err) {
     console.log('Running server on port: ' + port);
