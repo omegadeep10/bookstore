65ec812e356e2dd31c24db5a5f8227c6a19601da
omegadeep10 <omegadeep10@outlook.com>
2016-07-25 19:48:43 -0400

    Implement cart add/remove functionality, and subject sort

--- src/routes/storeRoutes.js
+++ src/routes/storeRoutes.js
@@ -14,6 +14,7 @@ function ensureAuthenticated(req, res, next) {
 }
 
 authRouter.get('/', ensureAuthenticated, function(req, res) {
+    //ensures all queries complete before res.render
     async.parallel({
         books: function(callback) {
                 db.query('SELECT * FROM books LIMIT 20', function(error, results, fields) {
@@ -36,7 +37,71 @@ authRouter.get('/', ensureAuthenticated, function(req, res) {
                 });
             },
         subjects: function(callback) {
-                db.query('SELECT DISTINCT subject FROM books;', function(error, results, fields) {
+                db.query('SELECT DISTINCT subject FROM books ORDER BY subject;', function(error, results, fields) {
+                    if (error) {
+                        callback(error);
+                    }
+                    else {
+                        callback(null, results);
+                    }
+                });
+            }
+    }, function(error, results) {
+        res.render('store', {
+            error_msg: req.flash('error_msg'), 
+            success_msg: req.flash('success_msg'), 
+            user: req.user,
+            books: results.books,
+            cart: results.cart,
+            subjects: results.subjects
+        });
+    });
+});
+
+//removes a specific cart entry
+authRouter.get('/cart/remove/:cart_id', ensureAuthenticated, function(req, res) {
+    var cart_id = req.params.cart_id;
+    db.query('DELETE FROM cart WHERE cart_id = ?', [cart_id], function(error, results, fields) {
+        if (error) throw error;
+    });
+    res.redirect('/store');
+});
+
+//adds cart entry OR updates the quantity += one if the book is already in the user's cart
+authRouter.get('/cart/add/:book_id', ensureAuthenticated, function(req, res) {
+    var book_id = req.params.book_id;
+    db.query('CALL addToCart(?, ?);', [req.user.email, book_id], function(error, results, fields) {
+        if (error) throw error;
+    });
+    res.redirect('/store');
+});
+
+//returns filtered list of books, sorted by subjects
+authRouter.get('/sort/:subject', ensureAuthenticated, function(req, res) {
+    var subject = req.params.subject;
+    async.parallel({
+        books: function(callback) {
+                db.query('SELECT * FROM books WHERE subject = ? LIMIT 20', [subject], function(error, results, fields) {
+                    if (error) {
+                        callback(error);
+                    }
+                    else {
+                        callback(null, results);
+                    }
+                });
+            },
+        cart: function(callback) {
+                db.query('CALL getCartForUserEmail(?);', [req.user.email], function(error, results, fields) {
+                    if (error) {
+                        callback(error);
+                    }
+                    else {
+                        callback(null, results);
+                    }
+                });
+            },
+        subjects: function(callback) {
+                db.query('SELECT DISTINCT subject FROM books ORDER BY subject;', function(error, results, fields) {
                     if (error) {
                         callback(error);
                     }
